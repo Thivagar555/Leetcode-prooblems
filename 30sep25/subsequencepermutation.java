b546323f19598ac86499d61faf927acf5cd33b2b
Thivagar555 <thivagar1610@gmail.com>
2025-09-30 13:33:59 +0530

    done

--- 30sep25/subsequencepermutation.java
+++ 30sep25/subsequencepermutation.java
@@ -4,35 +4,31 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class codeForces {
     public static void main(String[] args) throws IOException {
-        
+          // TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
 		
 		while(t-- >0)
 		{
 			int  num = Integer.parseInt(br.readLine());
-			if(num < 100) 
-				{
-				System.out.println(num%10);
-				
-				}
-			else
+			String value = br.readLine();
+			char[] arr =  value.toCharArray();
+			Arrays.sort(arr);
+			int count = 0;
+			for(int i=0; i<arr.length; i++)
 			{
-				int minimum = Integer.MAX_VALUE;
-				int temp = num;
-				while(temp > 0)
-				{
-					int digit = temp%10;
-					minimum = Math.min(digit, minimum);
-					temp /= 10;
-				}
-				System.out.println(minimum);
+				if(value.charAt(i) != arr[i]) count++;
+				
 			}
+			System.out.println(count);
 			
 		}
+		
     }
 }
