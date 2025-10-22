97b4925b42c03ddcbbd486b748b63bfe1cd5fc6b
Thivagar555 <thivagar1610@gmail.com>
2025-10-22 17:21:13 +0530

    done

---
+++ 22oct25/820.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        int count =0;
+        for(String word : words)
+        {
+            for(int i =1; i<word.length(); i++)
+            {
+                set.remove(word.substring(i));
+            }
+            // System.out.println()
+          
+                // count += word.length()+1;
+            
+        }
+        for(String word : set)
+        {
+            count += word.length()+1;
+        }
+        return count;
+        // for(String word : words)
+    }
+}
