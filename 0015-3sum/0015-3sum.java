5e42062169e9402ce57a3089423f971042850629
Thivagar555 <thivagar1610@gmail.com>
2026-01-20 12:27:20 +0530

    Time: 34 ms (58.25%), Space: 59.3 MB (52.16%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,61 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
+        // Result list to store all unique triplets
         List<List<Integer>> result = new ArrayList<>();
+
+        // Step 1: Sort the array
         Arrays.sort(nums);
 
         int n = nums.length;
 
-        for(int i=0; i<n-2; i++)
-        {
-            int left = i+1;
-            int right = n-1;
+        // Step 2: Fix one number nums[i]
+        for (int i = 0; i < n - 2; i++) {
 
-            if(i>0 && nums[i] == nums[i-1]) continue;
+            // Skip duplicate elements for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-            while(left < right)
-            {
+            // Two pointers
+            int left = i + 1;
+            int right = n - 1;
+
+            // Step 3: Two-pointer approach
+            while (left < right) {
+
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if(sum == 0)
-                {
-                    result.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+                if (sum == 0) {
+                    // Found a valid triplet
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    while(left < right && nums[left]== nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right --;
+                    // Skip duplicates for left pointer
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicates for right pointer
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    // Move both pointers
                     left++;
                     right--;
-                }
-                else if(sum > 0)
-                {
-                    right--;
-                }
-                else
-                {
+
+                } else if (sum < 0) {
+                    // Need a larger sum
                     left++;
+                } else {
+                    // Need a smaller sum
+                    right--;
                 }
             }
         }
+
         return result;
     }
 }
