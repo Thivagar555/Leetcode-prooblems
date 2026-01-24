cea43209fe268875615e4cc688775f95f2bd98f4
Thivagar555 <thivagar1610@gmail.com>
2026-01-24 09:15:00 +0530

    Time: 46 ms (8.16%), Space: 56.3 MB (90.1%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -4,23 +4,23 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         map.put(0,1);
-        int count = 0;
+        int count = 0; 
         int prefixSum = 0;
 
         for(int i : nums)
         {
             if(i%2 != 0)
-            {
-                prefixSum ++;
-            }
+            prefixSum++;
 
-            if(map.containsKey(prefixSum-k))
+            if(map.containsKey(prefixSum - k))
             {
-                count += map.get(prefixSum-k);
+                count += map.get(prefixSum - k);
             }
 
             map.put(prefixSum, map.getOrDefault(prefixSum, 0)+1);
+
         }
+
         return count;
     }
 }
