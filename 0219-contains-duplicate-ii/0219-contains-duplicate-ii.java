8a1c89372af12d8370e3face6f99482f2bae8418
Thivagar555 <thivagar1610@gmail.com>
2026-01-30 09:10:01 +0530

    Time: 23 ms (82.32%), Space: 76.1 MB (88.7%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int left = 0;
         Set<Integer> set = new HashSet<>();
 
-
+        //fixed size Sliding window + hash set logic
         for(int right = 0; right< nums.length; right++)
         {
             //if length exceed our window k shrink left 
