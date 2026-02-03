860fab0e4f36cec042db5c19274a7e8cc8289771
Thivagar555 <thivagar1610@gmail.com>
2026-02-03 09:08:50 +0530

    Time: 0 ms (100%), Space: 44.7 MB (49.58%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -3,41 +3,57 @@ class Solution {
         int n = nums.length;
         if (n < 4) return false;
 
-        int p = -1, q = -1;
+        // int p = -1, q = -1;
 
-        // 1️⃣ Find p (increase -> decrease)
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] >= nums[i + 1]) {
-                p = i;
-                break;
-            }
-        }
-        if (p <= 0) return false;
+        // // 1️⃣ Find p (increase -> decrease)
+        // for (int i = 0; i < n - 1; i++) {
+        //     if (nums[i] >= nums[i + 1]) {
+        //         p = i;
+        //         break;
+        //     }
+        // }
+        // if (p <= 0) return false;
 
-        // 2️⃣ Validate strictly increasing [0..p]
-        for (int i = 0; i < p; i++) {
-            if (nums[i] >= nums[i + 1]) return false;
-        }
+        // // 2️⃣ Validate strictly increasing [0..p]
+        // for (int i = 0; i < p; i++) {
+        //     if (nums[i] >= nums[i + 1]) return false;
+        // }
 
-        // 3️⃣ Find q (decrease -> increase)
-        for (int i = p; i < n - 1; i++) {
-            if (nums[i] <= nums[i + 1]) {
-                q = i;
-                break;
-            }
-        }
-        if (q == -1 || q == p) return false;
+        // // 3️⃣ Find q (decrease -> increase)
+        // for (int i = p; i < n - 1; i++) {
+        //     if (nums[i] <= nums[i + 1]) {
+        //         q = i;
+        //         break;
+        //     }
+        // }
+        // if (q == -1 || q == p) return false;
 
-        // 4️⃣ Validate strictly decreasing [p..q]
-        for (int i = p; i < q; i++) {
-            if (nums[i] <= nums[i + 1]) return false;
-        }
+        // // 4️⃣ Validate strictly decreasing [p..q]
+        // for (int i = p; i < q; i++) {
+        //     if (nums[i] <= nums[i + 1]) return false;
+        // }
 
-        // 5️⃣ Validate strictly increasing [q..n-1]
-        for (int i = q; i < n - 1; i++) {
-            if (nums[i] >= nums[i + 1]) return false;
-        }
+        // // 5️⃣ Validate strictly increasing [q..n-1]
+        // for (int i = q; i < n - 1; i++) {
+        //     if (nums[i] >= nums[i + 1]) return false;
+        // }
 
-        return true;
+        // return true;
+
+
+        int i=1;
+
+        while(i<n && nums[i-1] < nums[i]) i++;
+        if(i==1 || i==n) return false;
+
+        int p=i;
+        while(i<n && nums[i-1] > nums[i]) i++;
+        if(i==p || i==n) return false;
+
+        while(i<n && nums[i-1] < nums[i]) i++;
+
+            return i==n;
+        
+
     }
 }
