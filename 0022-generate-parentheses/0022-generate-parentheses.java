c71505a863d7599077e667de4a50c943dcba1694
Thivagar555 <thivagar1610@gmail.com>
2026-02-04 10:11:56 +0530

    Time: 2 ms (66.67%), Space: 44.5 MB (76.28%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        //backtracking 
+        List<String> result = new ArrayList<>();
+
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+    private static void backtrack(List<String> result, String current, int open, int close, int n)
+    {
+        //if the n=3 then it will be 3 open brackets and 3 closed so it is 2*n;
+        if(current.length()==2*n)
+        {
+            result.add(current);
+            return;
+        }
+        
+       // Try to add '(' if we still can
+        if(open < n)
+        {
+            backtrack(result, current + "(", open+1, close, n);
+        }
+        
+        // Try to add ')' if we still can
+        if(close < open)
+        {
+            backtrack(result, current + ")", open, close+1, n);
+        }
+    }
+}
