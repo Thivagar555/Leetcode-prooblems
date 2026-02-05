aaec420abc35274935752e724185f155e7f3e913
Thivagar555 <thivagar1610@gmail.com>
2026-02-05 08:49:46 +0530

    Time: 0 ms (100%), Space: 42 MB (90.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,29 +1,26 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+       int[][] dp = new int[m][n];
 
-        //first row can be have only one path
-        for(int i=0; i<n; i++)
+        //first row has only one way
+        for(int i=0; i< m; i++)
         {
-            dp[0][i] = 1;
+            dp[i][0] = 1;
         }
-
-        //first col can be have only one path downward
-        for(int j=1; j<m; j++)
-        {
-            dp[j][0] = 1;
+        //first column also contains only one way
+        for(int j=1; j<n; j++)
+         {
+            dp[0][j] = 1;
         }
-
-        //core Logic : addition of path ways from top and the left 
-        for(int i =1; i<m; i++)
+        //core logic= current box can be achieved in both way - one from up and another from left so we are going to add the both up and left boxes in current
+        for(int i =1;i<m; i++)
         {
             for(int j=1; j<n; j++)
-            {
-                // Robot can come from TOP or LEFT
-                dp[i][j]= dp[i-1][j]+dp[i][j-1];
-            }
+            {           //up        +   left
+                dp[i][j]= dp[i-1][j] + dp[i][j-1];
+            } 
         }
-        //last box stores the answer 
+        //last coln stores answers
         return dp[m-1][n-1];
     }
 }
