973ea93cf035c84ac2aeb9c52557e85f893f76c1
Thivagar555 <thivagar1610@gmail.com>
2026-02-08 18:18:58 +0530

    Time: 0 ms (100%), Space: 42 MB (83.91%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,17 @@
 class Solution {
     public int fib(int n) {
+        //dp approach
+        
         if(n==0) return 0;
+
+
+        //n+1 because [0, 1, ....] these two are default values in fibinocci
+
         int[] dp = new int[n+1];
 
-        dp[1] = 1;
+        dp[1] = 1;//default
 
+        //current no is addition of past two
         for(int i =2 ; i<n+1; i++)
         {
             dp[i] =  dp[i-1]+dp[i-2];
