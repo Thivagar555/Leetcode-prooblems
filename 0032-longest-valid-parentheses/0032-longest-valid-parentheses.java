5259ea07ae7d44de5cc4a10063a9c7685d8a2c24
Thivagar555 <thivagar1610@gmail.com>
2026-02-09 09:59:38 +0530

    Time: 7 ms (7.96%), Space: 46.4 MB (75.45%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -2,7 +2,14 @@ class Solution {
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
 
-        //
+        //-1 because if the valid paranthesis start from zeroth index then it will help to calculate int len = i-stack.peek(); 
+        //for eg: 4-(-1) = 5 
+
+        // core logic = pushing the index of parenthesis if it equals = ( and pop if it equals = )
+        //if stack is empty set the current index as base
+        //or else the stack contains valid parenthesis find the len by doing int len = i-stack.peek();
+
+
         stack.push(-1);
 
         int maxLen = 0;
