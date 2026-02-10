51fb387e66dcc165a9065fedb0e6ba3d58523a58
Thivagar555 <thivagar1610@gmail.com>
2026-02-10 11:44:23 +0530

    Time: 1 ms (99.59%), Space: 71.6 MB (38.16%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        //core logic : there must be 1 to n (n be the size of the arr) elements in the array
+
+        //nums[x-1] = x - this is only for 1...n
+
+        // if all the 1....n present then the answer must be n+1;
+
+        //ignore negatives and zeros
+
+        int n = nums.length;
+
+        for(int i = 0; i<n; i++)
+        {
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1] != nums[i])
+            {
+                int x = nums[i];
+                int correctIdx = x-1;
+                int temp = nums[x-1];
+
+
+                nums[correctIdx] =x;
+                nums[i] = temp;
+            }
+        }
+        for(int i=0; i<n; i++)
+        {
+           if(nums[i] != i+1)
+           return i+1;
+        }
+        return n+1;
+    }
+}
